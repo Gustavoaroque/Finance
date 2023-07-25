f61da151f919f21e35a611989ed6eb65aac51736
Gustavo <gustavo.roque.143@gmail.com>
2023-07-25 12:12:34 -0600

    first section

--- FinanceApp/views.py
+++ FinanceApp/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def home(request):
-    
+    total_amount_expenses = 0
+    total_amount_incomes = 0
+    Expenses = Transaction.objects.filter(transaction_type='Expense')
+    Incomes = Transaction.objects.filter(transaction_type='Income')
+    print(Expenses)
+    print('=====================')
+    print(Incomes)
+
+    for Ex in Expenses:
+        total_amount_expenses = Ex.transaction_amount + total_amount_expenses
+    for In in Incomes:
+        total_amount_incomes = In.transaction_amount + total_amount_incomes
     context = {
-        'title':'Home Page'
+        'title':'Home Page',
+        'expenses_total':total_amount_expenses,
+        'incomes_total':total_amount_incomes,
     }
-    return render(request, 'main.html', context)
+    return render(request, 'HomePage.html', context)
